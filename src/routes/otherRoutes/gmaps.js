6747bde0bff2732964670d7ab5eead9f1777aa84
alexis-savoie <alexis.savoie.555@gmail.com>
2020-12-13 14:31:09 +0100

    finished /getRoute

--- src/routes/otherRoutes/gmaps.js
+++ src/routes/otherRoutes/gmaps.js
@@ -14,7 +14,7 @@ const gmaps = require("express").Router()
 
 
 //#region Get shortest itinery for a set of steps
-changePassword.get("/getRoute/:token/:origin/:waypoints", middleware.middlewareSessionUser, (req, res) => {
+gmaps.get("/getRoute/:token/:origin/:waypoints", middleware.middlewareSessionUser, async(req, res) => {
         // Get request params
         const origin = req.params.origin;
         let dests = req.params.waypoints.split('|');
@@ -46,75 +46,99 @@ changePassword.get("/getRoute/:token/:origin/:waypoints", middleware.middlewareS
         listDuration = []
 
 
-        console.log(routes)
+        //console.log(routes)
 
 
-
-
-
-
-        // Use a calback because request are async
-        function getRoutes(routes, callback) {
-            for (let i = 0; i < routes.length; i++) {
-
-                axios.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + routes[i][1] + '&mode=driving&key=' + key + '&waypoints=' + routes[i][0] + '&region=FR&language=fr&optimizeWaypoints=true')
-                    .then(res => {
-                        resRoutes.push(res.data)
-
-                        i = 0
-                        totalTime = 0
-                        while (i < res.data.routes[0].legs.length) {
-                            totalTime += res.data.routes[0].legs[i].duration.value
-                            i = i + 1
-                        }
-
-                        listDuration.push(totalTime)
-                        console.log(totalTime)
-                        console.log(routes)
-                            //console.log('https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + routes[i][1] + '&mode=driving&key=' + key + '&waypoints=' + routes[i][0] + '&region=FR&language=fr&optimizeWaypoints=true')
-
-
-
-
-                        // Once every route was obtained we start the callcback
-                        if (listDuration[routes.length - 1] != undefined) {
-                            console.log("done")
-                            callback(resRoutes, listDuration);
-                        }
-
-
-                    })
-                    .catch(error => {
-                        if (error.reponse) {
-                            console.log(error.response.data)
-
-                        } else {
-                            console.log(error)
-                        }
-
-                    })
-
-            }
+        // We need to made the request async to properly wait for the request response
+        async function httpRequest(origin, destination, key, waypoints) {
+            return axios({
+                method: "get",
+                url: 'https://maps.googleapis.com/maps/api/directions/json?origin=' + origin + '&destination=' + destination + '&mode=driving&key=' + key + '&waypoints=' + waypoints + '&region=FR&language=fr&optimizeWaypoints=true',
+            }).then(res => res).catch(error => error);
         }
 
-        // Wait for all requests to finish using a callback
-        // And then compare what is the fastest route
-        getRoutes(routes, function(resRoutes, listDuration) {
 
-            let index = 0;
-            let value = listDuration[0];
-            for (let i = 1; i < listDuration.length; i++) {
-                if (listDuration[i] < value) {
-                    value = listDuration[i];
-                    index = i;
+        let success = true
+        for (let i = 0; i < routes.length; i++) {
+
+            // MAke the request and wait for the response
+            let res2 = await httpRequest(origin, routes[i][1], key, routes[i][0])
+
+            console.log(res2.status)
+
+            // INvalid adress case
+            if (res2.data.status == "NOT_FOUND" && success == true) {
+                console.log(1)
+                success = false
+                sr.sendReturn(res, 404, {
+                    error: true,
+                    message: "Location not found"
+                });
+            }
+            // SUccess case
+            else {
+                if ((res2.data.routes[0])) {
+                    console.log(2)
+                        // Store response data
+                    resRoutes.push(res2.data)
+
+                    // Calculate the duration of this itinerary
+                    let j = 0
+                    totalTime = 0
+                    while (j < res2.data.routes[0].legs.length) {
+                        totalTime += res2.data.routes[0].legs[j].duration.value
+                        j = j + 1
+                    }
+
+                    // Store the duration
+                    listDuration.push(totalTime)
+
+
+                    // Find fastest itinerary
+                    let index = 0;
+                    let value = listDuration[0];
+                    for (let k = 1; k < listDuration.length; k++) {
+                        if (listDuration[k] < value) {
+                            value = listDuration[k];
+                            index = k;
+                        }
+                    }
+
+                    // Save itinerary for the API response
+                    order = routes[index][0].toString() + "|" + routes[index][1].toString()
+                    shortestRoute = resRoutes[index]
+                    console.log(order)
+
+
+                }
+                // Failure case
+                else {
+                    if (success == true) {
+                        console.log(3)
+                        success = false
+                        sr.sendReturn(res);
+                    }
+
+
                 }
             }
 
-            console.log(listDuration)
-            console.log(routes[index][0] + "|" + routes[index][1])
-            console.log(listDuration[index])
-            console.log(resRoutes[index])
-        });
+
+
+
+        }
+        if (success == true) {
+            sr.sendReturn(res, 200, {
+                error: false,
+                message: "Shortest route found",
+                order: order,
+                route: shortestRoute
+            });
+        }
+
+
+
+
 
 
 
